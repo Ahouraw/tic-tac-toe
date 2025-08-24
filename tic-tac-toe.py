f92ae149fa991d2c95f4edbd823487a4858393cd
Ahoora <ahoura.78@gmail.com>
2025-08-23 21:08:14 -0400

    Implemented win_or_draw()

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -38,7 +38,6 @@ def main():
 
             # Let the player know what the CPU chose
             print(f"CPU selects square {action}")
-            print_board()
             # Update board with the action
             update_board(action, cpu)
             # Check for win or draw:
@@ -46,13 +45,18 @@ def main():
                 break
 
 
-def choose_action():
+def choose_action() -> str:
+    ... # TODO
+
+
+def congratulate_winner(winner: str) -> None:
     ... # TODO
 
 
 # Print current state of global board
 def print_board():
     print("Current state:\n")
+
     for index, row in enumerate(board):
         print("|".join(row))
         if index < 2:
@@ -60,7 +64,7 @@ def print_board():
 
 
 # Figure out whose turn it is, assuming X always goes first
-def whose_turn():
+def whose_turn() -> str:
     X_count = O_count = 0
     for row in board:
         for square in row:
@@ -86,25 +90,65 @@ def valid_action(action: str) -> bool:
     # Check if action is a number between 1 and 9
     if int(action) not in [i for i in range(1, 10)]:
         return False
+    
     # Check if the square is already taken
     row = (int(action) - 1 // 3)
     column = (int(action) - 1 % 3)
+
+    # if the square is not empty, aka "   ", return False
     if board[row][column] != "   ":
         print("Square already taken.")
         return False
+    
     return True
 
 
 def update_board(action: str, user_symbol: str) -> None:
+    # Get the coordinates of the action
     row = (int(action) - 1 // 3)
     column = (int(action) - 1 % 3)
 
+    # Update the board with the action
     global board
     board[row][column] = f" {user_symbol} "
+    
+    print_board()
 
 
 def win_or_draw():
-    ... # TODO
+    # Check rows for win
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] != "   ":
+            winner = row[0].strip()
+            congratulate_winner(winner)
+            return True
+
+    # Check columns for win
+    for column in range(3):
+        if board[0][column] == board[1][column] == board[2][column] and board[0][column] and board[0][column] != "   ":
+            winner = board[0][col].strip()
+            congratulate_winner(winner)
+            return True
+
+
+    # Check diagonals for win
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != "   ":
+        winner = board[0][0].strip()
+        congratulate_winner(winner)
+        return True
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != "   ":
+        winner = board[0][2].strip()
+        congratulate_winner(winner)
+        return True
+
+    # Check for draw
+    for row in board:
+        for square in row:
+            if square == "   ":
+                return False
+    return True
+
 
 if __name__ == "__main__":
     main()
